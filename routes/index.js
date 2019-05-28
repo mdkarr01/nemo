8a6012403c42a652b668abce627e89da01057a83
Michael Karr <mdkarr01@gmail.com>
2019-05-28 18:17:25 -0500

    Add user routes to index route page

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,75 @@ router.get('/', (req, res, next) => {
   });
 });
 
+// REGISTER A USER
+
+/* GET /register. */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST /register. */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+//===============================
+
+// LOGIN A USER
+
+/* GET /login. */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST /login. */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+//=================================
+
+// USERS PROFILE
+
+/* GET /profile. */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+// PUT /profile/:users_id 
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id');
+});
+
+/* DESTROY /profile/:user_id. */
+router.delete('/profile/:user_id', (req, res, next) => {
+  res.send('DESTROY /profile/:user_id');
+});
+
+//========================================
+
+// USER PASSWORDS
+
+/* GET /forgot-password. */
+router.get('/forgot-pw', (req, res, next) => {
+  res.send('GET /forgot-pw');
+});
+
+// PUT /forgot-password
+router.put('/forgot-pw', (req, res, next) => {
+  res.send('PUT /forgot-pw');
+});
+
+/* GET /reset-password/:token. */
+router.get('/reset-pw/:token', (req, res, next) => {
+  res.send('GET /reset-pw/:token');
+});
+
+// PUT /reset-password/:token
+router.put('/reset-pw/:token', (req, res, next) => {
+  res.send('PUT /reset-pw/:token');
+});
+
+//=======================================
+
 module.exports = router;
