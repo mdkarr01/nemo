e56a08a286180e891de5ceb54f120ad35ef3cdf7
Michael Karr <mdkarr01@gmail.com>
2019-06-05 17:15:11 -0500

    changed errorHandler to asyncErrorHandler

--- routes/posts.js
+++ routes/posts.js
@@ -9,26 +9,26 @@ const {
   updatePost
 } = require('../controllers/posts');
 const {
-  errorHandler
+  asyncErrorHandler
 } = require('../middleware');
 
 /* GET posts index /posts. */
-router.get('/', errorHandler(getPosts));
+router.get('/', asyncErrorHandler(getPosts));
 
 /* GET posts new /posts/new. */
 router.get('/new', newPost);
 
 /* POST posts create /posts */
-router.post('/', errorHandler(createPost));
+router.post('/', asyncErrorHandler(createPost));
 
 /* GET posts show /posts/:id */
-router.get('/:id', errorHandler(showPost));
+router.get('/:id', asyncErrorHandler(showPost));
 
 /* GET posts edit /posts/:id/edit. */
-router.get('/:id/edit', errorHandler(editPost));
+router.get('/:id/edit', asyncErrorHandler(editPost));
 
 /* PUT posts update /posts/:id */
-router.put('/:id', errorHandler(updatePost));
+router.put('/:id', asyncErrorHandler(updatePost));
 
 /* SDELETE posts destroy /posts/:id */
 router.delete('/:id', (req, res, next) => {
