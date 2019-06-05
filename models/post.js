d8e7409ad62186d0abab847db91ab1c1d7a9eb40
Michael Karr <mdkarr01@gmail.com>
2019-06-04 19:17:34 -0500

    changed cost to price to match Ian

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   title: String,
-  cost: String,
+  price: String,
   description: String,
   images: [String],
   location: String,
