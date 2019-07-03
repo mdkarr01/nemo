656f624ddc0f328005bf9510749c07bde3cfc36c
Michael Karr <mdkarr01@gmail.com>
2019-07-03 12:43:01 -0500

    finished update and delete images from post

--- controllers/posts.js
+++ controllers/posts.js
@@ -50,9 +50,42 @@ module.exports = {
     //Find post by id
     let post = await Post.findByIdAndUpdate(req.params.id);
     //check if any images for deletion
-    if (req.body.image.length <= 4) {
-
+    if (req.body.deleteImages, req.body.deleteImages.length) {
+      //assign deleteImages from req.body to its own variable
+      let deleteImages = req.body.deleteImages;
+      //loop over deleteImages
+      for (const public_id of deleteImages) {
+        //delete images from cloudinary
+        await cloudinary.v2.uploader.destroy(public_id);
+        //delete image from post.images
+        for (const image of post.images) {
+          if (image.public_id === public_id) {
+            let index = post.images.indexOf(image);
+            post.images.splice(index, 1);
+          }
+        }
+      }
     }
+    //check if there are any new images to upload
+    if (req.files) {
+      //upload files
+      for (const file of req.files) {
+        let image = await cloudinary.v2.uploader.upload(file.path);
+        //add images to post.images array
+        post.images.push({
+          url: image.secure_url,
+          public_id: image.public_id
+        });
+      }
+    }
+    //update the post with any new properties
+    post.title = req.body.post.title;
+    post.description = req.body.post.description;
+    post.price = req.body.post.price;
+    post.location = req.body.post.location;
+    //save the updated post to the database
+    post.save();
+    //redirect to the show page
     res.redirect(`/posts/${post.id}`);
   },
   //Post Destroy
