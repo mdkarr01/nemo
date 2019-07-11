1c7c383ba86f751fa35a17633c28f10f0b710e03
Michael Karr <mdkarr01@gmail.com>
2019-07-11 16:29:27 -0500

    starting on reviews

--- models/review.js
+++ models/review.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
-  author: String,
+  body: String,
+  rating: Number,
   author: {
     type: Schema.Types.ObjectId,
     ref: "User"
