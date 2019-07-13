1a2946c896b672553507698e4d0dae9cd801c5c1
Michael Karr <mdkarr01@gmail.com>
2019-07-13 15:35:34 -0500

    added review auth middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -8,14 +8,14 @@ const {
   reviewDestroy
 } = require('../controllers/reviews');
 const {
-  asyncErrorHandler
+  asyncErrorHandler, isReviewAuthor
 } = require('../middleware');
 
 /* POST reviews create /posts/:id/reviews */
 router.post('/', asyncErrorHandler(reviewCreate));
 
 /* PUT reviews update /posts/:id/reviews/:review_id  */
-router.put('/:review_id', asyncErrorHandler(reviewUpdate));
+router.put('/:review_id', isReviewAuthor, asyncErrorHandler(reviewUpdate));
 
 /* DELETE reviews destroy /posts/:id/reviews/:review_id  */
 router.delete('/:review_id', asyncErrorHandler(reviewDestroy));
