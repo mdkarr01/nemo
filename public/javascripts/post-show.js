59604db48f82eade3f4d46bda7fd445a1fb97260
Michael Karr <mdkarr01@gmail.com>
2019-07-15 16:08:31 -0500

    removed clear ratings button
    js didn’t work

--- public/javascripts/post-show.js
+++ public/javascripts/post-show.js
@@ -16,13 +16,17 @@ el.className = 'marker';
 new mapboxgl.Marker(el)
   .setLngLat(post.coordinates)
   .setPopup(new mapboxgl.Popup({
-      offset: 25
-    }) // add popups
+    offset: 25
+  }) // add popups
     .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
   .addTo(map);
 
-  //Togggle edit form
+//Togggle edit form
 $('.toggle-edit-form').on('click', function () {
   $(this).text() === 'Edit' ? $(this).text('Cancel') : $(this).text('Edit');
   $(this).siblings('.edit-review-form').toggle();
+});
+
+$('.clear-rating').click(function () {
+  $(this).siblings('input-no-rate').click();
 });
